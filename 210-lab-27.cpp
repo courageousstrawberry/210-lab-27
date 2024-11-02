d4435a595f674c7bc0f7d0a1be4fd29ada1b3a18
courageousstrawberry <elech2a@gmail.com>
2024-11-01 20:50:26 -0700

    small fixes

--- 210-lab-27.cpp
+++ 210-lab-27.cpp
@@ -17,35 +17,7 @@ int main() {
     // note how the right-hand side of the assignment are the vector elements
     villagers["Audie"] = make_tuple(10, "Armadillo", "Let's climb together!");
     villagers["Raymond"] = make_tuple(3, "Cat", "I don't need any humans.");
-    villagers.insert({"Marshal", make_tuple(7, "Frog", "I want that fly!")});
-
-    // access the map using iterators
-    cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, vector<string>>::iterator it = villagers.begin(); 
-                                               it != villagers.end(); ++it) {
-        cout << it->first << ": ";
-        for (auto color : it->second) {
-            cout << color << " ";
-        }
-        cout << endl;
-    }
-
-    // search for an element using .find() to avoid errors
-    string searchKey = "Audie";
-    auto it = villagers.find(searchKey);
-    if (it != villagers.end()) {  // the iterator points to beyond the end of the map
-                                       // if searchKey is not found
-        cout << "\nFound " << searchKey << "'s favorite colors: ";
-        for (auto color : it->second)  // range loop to traverse the value/vector
-            cout << color << " ";
-        cout << endl;
-    } else
-        cout << endl << searchKey << " not found." << endl;
-
-    // report size, clear, report size again to confirm map operations
-    cout << "\nSize before clear: " << villagers.size() << endl;
-    villagers.clear();
-    cout << "Size after clear: " << villagers.size() << endl;
+    villagers.insert({"Marshal", make_tuple(7, "Frog", "Eat that fly!")});
 
     while(choice !=6) {
         choice = menu();
